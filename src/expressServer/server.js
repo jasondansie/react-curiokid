0b8e56591e1f1858e5264542e612588d585dc16d
jasondansie <jasondansie@gmail.com>
2022-11-25 11:03:30 +0200

    Revert "adds book library helper files for server"
    This reverts commit c9fa8d50360bb5ccd6e17a0d508fa63e824dbc60.

--- src/expressServer/server.js
+++ src/expressServer/server.js
@@ -1,49 +1,15 @@
 'use strict'
 
-const { getAllBooks, getBook } = require('./bookStorage');
-
-
 const http = require('http');
 
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3030;
-const host = process.env.HOST || "localhost";
-
+const port = 3000;
+const host = 'localhost';
 
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    const {
-        pathname,
-        searchParams
-    } = new URL(`http://${req.headers.host}${req.url}`);
-
-    const route = decodeURIComponent(pathname);
-
-        let result = [];
-
-        if (route === '/books') {
-            result = getAllBooks();
-        }
-        else if(route === '/search/byauthor'){
-            result = getBook('author', searchParams.get('value'));
-        }
-        else if(route === '/search/bytitle'){
-            result = getBook('title', searchParams.get('value'));
-        }
-        else{
-            result = {message: "page not found"};
-        }
-
-        res.writeHead(200,{ 
-            'Content-type': 'application/json',
-            'Access-Control-Allow-Origin':'*'
-        });
-
-        res.end(JSON.stringify(result,null,2));
-
-});
+app.get('/', (req, res) => res.send(`<h1>Hello World!!</h1>`));
 
 server.listen(port, host, () => console.log(`Server running on host:${host} port:${port}`))
