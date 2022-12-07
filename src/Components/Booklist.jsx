96cdeb03c376e3354171608a16642b523d09bc60
jasondansie <jasondansie@gmail.com>
2022-12-07 17:12:16 +0200

    add serch, not working in all books.

--- src/Components/Booklist.jsx
+++ src/Components/Booklist.jsx
@@ -9,10 +9,15 @@ const { getBooks } = require('../server/bookLibrary');
 
 const Booklist = () => {
     const params = useParams();
+    const [allBooksList, setAllBookList] = useState([]);
     const [bookList, setBookList] = useState([]);
+    const [search, setSearch] = useState({
+        string: '',
+    });
 
     useEffect(() => {
        
+
         const getBookList = (pageType) => {        
     
             if (pageType === "allbooks") {
@@ -25,19 +30,21 @@ const Booklist = () => {
                 showRangeOfBooks(8);
             }               
         }
-        getBookList(params.pageType);
-    }, [params.pageType])
-  
-    const getAllBooks =  () => {    
-        getBooks('http://localhost:3030/books').then((fetchedBookList) =>{   
-            setBookList(fetchedBookList);
-        })  
-    }
+
+        const getAllBooks =  () => {    
+            getBooks('http://localhost:3030/books').then((fetchedBookList) =>{   
+                setAllBookList(fetchedBookList);
+                setBookList(allBooksList);
+            })  
+        }
+        
+       getBookList(params.pageType);
+    }, [params.pageType, allBooksList]);
     
-    const showRangeOfBooks = (bookAge) => {          
-        let foundBooks = [];
-        getBooks('http://localhost:3030/books').then((fetchedBookList) =>{
-            
+    
+    const showRangeOfBooks = (bookAge) => {       
+        let foundBooks = [];    
+        getBooks('http://localhost:3030/books').then((fetchedBookList) =>{   
             fetchedBookList.forEach(book => {
                 if (bookAge === 7) {
                     if(book.age <= bookAge) {
@@ -48,29 +55,50 @@ const Booklist = () => {
                     foundBooks.push(book);
                 }             
             })
-            setBookList(foundBooks)                  
-        })   
+            setBookList(foundBooks)
+            })                           
     }
+
+    const searchHandler = (e) => {
+        setSearch({
+            ...search,
+            string: e.target.value,
+        });
+        let fB = allBooksList.filter(book => 
+            book.title.toLowerCase().includes(search.string) ||
+             book.author.toLowerCase().includes(search.string)
+             );         
+        console.log("foundbooks: ", fB);
+        setBookList(fB);
+           
+    }
+
+    const dispalyCards = () =>{
+       return bookList.map((book) => (
+            <Card
+            key = {book.id}
+            image= {book.image}
+            title= {book.title}
+            author= {book.author}
+            />
+        ))     
+    }           
 
     return (
         <div>
             <div className='app'> 
                 <Title />                   
                 <div className='inputs'>
-                    <Search />
+                    <Search
+                        {...search}
+                        searchHandler={searchHandler}
+                     />
                     <Link to="/allbooks"><Button name={"Books for all "} ></Button></Link>
                     <Link to="/age7"><Button name={"Books for age 5-7 "}></Button></Link>
                     <Link to="/age10"><Button name={"Books for age 8-10 "}></Button></Link>                 
-                </div>
+                </div>               
                 <div className="bookCards">
-                    {bookList.map((book) => (
-                        <Card
-                        key = {book.id}
-                        image= {book.image}
-                        title= {book.title}
-                        author= {book.author}
-                        />
-                    ))}     
+                  {dispalyCards()}                     
                 </div>                                                     
             </div>
         </div>
