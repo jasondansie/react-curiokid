0b7da31dbd2abedcbb5a53f74ca866806a70f5cb
jagriti99 <57299157+jagriti99@users.noreply.github.com>
2022-12-08 21:31:30 +0200

    images add

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link, Outlet} from "react-router-dom";
 import Button from './Button';
 import Title from './Title'
-import '../App.css'
+import '../App.css';
+
 
 const Home = () => {
  
@@ -11,11 +12,20 @@ const Home = () => {
             <Title />
             <section className='buttons'>
                 <Link to="/allbooks">
-                    <img src={`https://source.unsplash.com/500x400/?mammal`} alt="animal"/>
+                    <img src={`https://img.freepik.com/free-vector/books-with-children-white-background_1308-20924.jpg?w=2000`} alt="animal"/>
+                    <h2>Books for all</h2>
                     {/* <Button name={"Books for all "}></Button> */}
                     </Link>
-                <Link to="/age7"><Button name={"Books for age 5-7 "}></Button></Link>
-                <Link to="/age10"><Button name={"Books for age 8-10 "}></Button></Link>                 
+                <Link to="/age7">
+                    {/* <Button name={"Books for age 5-7 "}></Button> */}
+                    <img src={`https://i.pinimg.com/736x/fd/90/ce/fd90cec447319d7404ef841f1becb874.jpg`} alt="animal"/>
+                    <h2>Books for age 5-7</h2>
+                    </Link>
+                <Link to="/age10">
+                <img src={`https://as1.ftcdn.net/v2/jpg/01/45/94/38/1000_F_145943800_BwwpjBI2gf4vyJuEB5BpNsIyqCwHh7Xi.jpg`} alt="animal"/>
+                <h2>Books for age 8-10</h2>
+                    {/* <Button name={"Books for age 8-10 "}></Button> */}
+                    </Link>                 
             </section>             
             <Outlet /> 
         </section>
