b89b5a90ddc1acf0f57ff919efa5293893bf779f
jagriti99 <57299157+jagriti99@users.noreply.github.com>
2022-12-09 14:26:10 +0200

    add images to home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Link} from "react-router-dom";
-import Button from './Button';
+
 import Title from './Title'
 import './Home.css';
 
@@ -10,9 +10,21 @@ const Home = () => {
         <section className='home'>
             <Title />
             <section className='buttons'>
-                <Link to="/allbooks"><Button name={"Books for all "} ></Button></Link>
-                <Link to="/age7"><Button name={"Books for age 5-7 "}></Button></Link>
-                <Link to="/age10"><Button name={"Books for age 8-10 "}></Button></Link>                                
+                <Link to="/allbooks">
+                    <img src='https://img.freepik.com/free-vector/books-with-children-white-background_1308-20924.jpg?w=900&t=st=1670588465~exp=1670589065~hmac=a321df0f7f6975427d4fa530f43c89b7ff9681c25c35a26eefb560f502f1cca3' alt="kids"></img>
+                    <h2>Books for all</h2>
+                    
+                    </Link>
+                <Link to="/age7">
+                <img src='https://img.freepik.com/free-vector/children-reading-books-white-background_1308-99595.jpg?w=996&t=st=1670588346~exp=1670588946~hmac=b8163ddfe423d19acbc43987c6189b4398dfa1dde10aa53dfc5ac970331931a4' alt="kids"></img>
+                    <h2>Books for age 5-7</h2>
+                   
+                    </Link>
+                <Link to="/age10">
+                <img src='https://img.freepik.com/free-vector/boy-girl-reading-book_1308-50995.jpg?w=1800&t=st=1670588280~exp=1670588880~hmac=99ef4db58d786c1262c91bf1961829d728fb2e23abe19a36d9452a9617ef9de6' alt="kids"></img>
+                    <h2>Books for age 8-10</h2>
+                   
+                    </Link>                                
             </section>                           
         </section>
     );  
