63ecd86bcab5e2ce9e9ff9b36a1e9d22e1468fa9
Jason Dansie <jasondansie@gmail.com>
2022-12-16 09:20:51 +0200

    ladds code refactoring

--- src/Components/Booklist.jsx
+++ src/Components/Booklist.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link, useParams} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Search from './Search';
 import Button from './Button';
 import Title from './Title';
@@ -17,46 +17,53 @@ const Booklist = () => {
     });
 
     useEffect(() => {
+        setBookList([]);
+        setAllBookList([]);
+        getBooks('http://localhost:3030/books').then((fetchedBookList) => {
+            setAllBookList(fetchedBookList);
+        })
 
-        const getBookList = (pageType) => {        
-    
-            if (pageType === "allbooks") {
-                getAllBooks();            
-            }
-            if (pageType === "age7") {
-                showRangeOfBooks(7);
-            }
-            if (pageType === "age10") {
-                showRangeOfBooks(8);
-            }               
-        }
+    }, []);
 
-        const getAllBooks =  () => {  
-            getBooks('http://localhost:3030/books').then((fetchedBookList) =>{   
-                setAllBookList(fetchedBookList);
-                setBookList(allBooksList);      
-            })  
-        }
-        
-       getBookList(params.pageType);
-    }, [params.pageType, allBooksList]);
-    
-    const showRangeOfBooks = (bookAge) => {       
-        let foundBooks = [];   
-        getBooks('http://localhost:3030/books').then((fetchedBookList) =>{   
-            fetchedBookList.forEach(book => {
-                if (bookAge === 7) {
-                    if(book.age <= bookAge) {
-                        foundBooks.push(book);
-                    }  
-                }
-                else if (book.age >= 8 && book.age <= 10) {
+    const buttonHandler = () => {
+        console.log("pagetype: ", params.pageType);
+        getBookList(params.pageType);
+    }
+
+    const getAllBooks = () => {
+        console.log("setting all books");
+        console.log("foundbooks", allBooksList);
+        setBookList([]);
+        setBookList(allBooksList);
+    }
+
+    const showRangeOfBooks = (bookAge) => {
+        let foundBooks = [];
+        setBookList([]);
+        allBooksList.forEach(book => {
+            if (bookAge === 7) {
+                if (book.age <= bookAge) {
                     foundBooks.push(book);
-                }             
-            })
-            setBookList(foundBooks)
-          
-            })                           
+                }
+            }
+            else if (book.age >= 8 && book.age <= 10) {
+                foundBooks.push(book);
+            }
+        })
+        setBookList(foundBooks);
+    }
+
+    const getBookList = (pageType) => {
+
+        if (pageType === "allbooks") {
+            getAllBooks();
+        }
+        if (pageType === "age7") {
+            showRangeOfBooks(7);
+        }
+        if (pageType === "age10") {
+            showRangeOfBooks(8);
+        }
     }
 
     const searchHandler = (e) => {
@@ -64,41 +71,43 @@ const Booklist = () => {
             ...search,
             string: e.target.value,
         });
-        let foundBooks = allBooksList.filter(book => 
+        let foundBooks = allBooksList.filter(book =>
             book.title.toLowerCase().includes(search.string.toLowerCase()) ||
-             book.author.toLowerCase().includes(search.string.toLowerCase())
-             );         
-        setBookList(foundBooks);     
+            book.author.toLowerCase().includes(search.string.toLowerCase())
+        );
+        setBookList(foundBooks);
     }
 
-    const dispalyCards = () =>{
-       return bookList.map((book) => (
+    const displayCards = () => {
+        return bookList.map((book) => (
             <Card
-            key = {book.id}
-            image= {book.image}
-            title= {book.title}
-            author= {book.author}
+                key={book.id}
+                image={book.image}
+                title={book.title}
+                author={book.author}
             />
-       ))}
+        ))
+    }
 
-    return (     
-        <section className='bookList'> 
-            <Title />                   
+    return (
+        <section className='bookList'>
+            <Title />
             <section className='inputs'>
                 <Search
                     {...search}
                     searchHandler={searchHandler}
-                    />
-                <Link to="/allbooks"><Button name={"Books for all "} ></Button></Link>
-                <Link to="/age7"><Button name={"Books for age 5-7 "}></Button></Link>
-                <Link to="/age10"><Button name={"Books for age 8-10 "}></Button></Link>                 
-            </section>               
+                />
+                <Link to="/allbooks" onClick={buttonHandler}><Button name={"Books for all "} ></Button></Link>
+                <Link to="/age7" onClick={buttonHandler}><Button name={"Books for age 5-7 "}></Button></Link>
+                <Link to="/age10" onClick={buttonHandler}><Button name={"Books for age 8-10 "}></Button></Link>
+            </section>
             <section className="bookCards">
-                {dispalyCards()}                     
-            </section>                                                     
+                {displayCards()}
+            </section>
         </section>
-       
-    );  
-} 
+
+    );
+
+}
 
 export default Booklist;
